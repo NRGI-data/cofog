d976235de4d97e0d66aa16cfb3f24df6dc2790be
Chris Perry <byndcivilization@gmail.com>
2015-08-05 17:48:59 -0400

    SAVEPOINT

--- scripts/update.sh
+++
@@ -1,7 +0,0 @@
-
-sudo pip install -r requirements.txt
-sudo apt-get install -y mdbtools
-python process.py retrieve
-python process.py export_to_csv
-python process.py export_to_rdf
-sudo rm -r cache
